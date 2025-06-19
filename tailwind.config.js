161ffdc23b5aa947abead18f43cf129f1399f4f0
placepharm <placepharm@gmail.com>
2025-06-19 14:24:51 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,28 @@ module.exports = {
   future: {
     hoverOnlyWhenSupported: true,
   },
+  content: [
+    './**/*.html',
+    './**/*.js',
+    './**/*.jsx',
+    './**/*.ts',
+    './**/*.tsx',
+  ],
   theme: {
     extend: {
       screens: {
         'hover-hover': { raw: '(hover: hover) and (pointer: fine)' },
       },
+      scale: {
+        125: '1.25', // group-hover:scale-125 지원
+      },
+      brightness: {
+        110: '1.1',  // group-hover:brightness-110 지원
+      },
+      contrast: {
+        110: '1.1',  // group-hover:contrast-110 지원
+      },
     },
   },
-  content: ['**/*.html'],
+  plugins: [],
 };
